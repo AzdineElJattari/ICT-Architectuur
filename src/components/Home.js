4f20a221546ad5fba59c6d4fb39c7b25b5ab37a0
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-03-17 19:04:42 +0100

    Small changes - Homepage

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export default function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Bestand uploaden
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
