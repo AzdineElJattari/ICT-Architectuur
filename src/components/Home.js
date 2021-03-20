081e8065ce0f37c96cb0dab1e700387fc1ec7c3a
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-03-20 18:44:19 +0100

    Added connection to API with Axios (Logs + files)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { Button, FormGroup, Modal, Form, Card, CardGroup} from "react-bootstrap";
+import axios from "axios";
+import {
+  Button,
+  Modal,
+  Form,
+  Card,
+  CardGroup,
+  Table,
+  Spinner,
+  Container,
+  Row,
+} from "react-bootstrap";
+
+const UserRow = ({ item, onClick }) => (
+  <tr onClick={onClick}>
+    <td>{item.uploader}</td>
+    <td>{item.filename}</td>
+    <td>{item.DocID}</td>
+    <td>{item.checksum}</td>
+  </tr>
+);
 
 export default function Home() {
+  const [files, setFiles] = useState([]);
+  const [logs, setLogs] = useState([]);
+  const [selectedLog, setSelectedLog] = useState(
+    "1def78ca-daa0-4eb3-8813-0329dedb5065"
+  );
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [isCheckedUpload, setisCheckedUpload] = useState(false);
   const [isCheckedDownload, setisCheckedDownload] = useState(false);
 
+  useEffect(() => {
+    async function fetchData() {
+      var getFiles =
+        "https://ohpj90916c.execute-api.us-east-1.amazonaws.com/getFiles?user=Laurens&api=PXW64JdS4dP2HJNQFt9D";
+      var getLogs = `https://ohpj90916c.execute-api.us-east-1.amazonaws.com/getLogs?user=Laurens&api=PXW64JdS4dP2HJNQFt9D&id=${selectedLog}`; //Moet nog aangepast  worden
+
+      const requestOne = axios.get(getFiles);
+      const requestTwo = axios.get(getLogs);
+      axios
+        .all([requestOne, requestTwo])
+        .then(
+          axios.spread((...responses) => {
+            const responseOne = responses[0];
+            const responseTwo = responses[1];
+
+            setFiles(responseOne.data.Items);
+            setLogs(responseTwo.data.Items);
+            setLoading(false);
+          })
+        )
+        .catch((errors) => {
+          console.error(errors);
+        });
+    }
+    fetchData();
+  }, [selectedLog]);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  function handleUserClick(item) {
+    setSelectedLog(item.DocID);
+  }
 
   return (
     <div>
@@ -18,14 +75,29 @@ export default function Home() {
               <Form.Group>
                 <Form.Label>Upload</Form.Label>
 
-                <Form.Group controleId="formPasswordUpload">
+                <Form.Group controlId="formPasswordUpload">
                   <Form.Group controlId="formPasswordSwitchUpload">
-                    <Form.Check type="switch" label={isCheckedUpload ? "Password enabled" : "Password disabled" } checked={isCheckedUpload} onChange={(e)=>{setisCheckedUpload(e.target.checked)}} />
+                    <Form.Check
+                      type="switch"
+                      label={
+                        isCheckedUpload
+                          ? "Password enabled"
+                          : "Password disabled"
+                      }
+                      checked={isCheckedUpload}
+                      onChange={(e) => {
+                        setisCheckedUpload(e.target.checked);
+                      }}
+                    />
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPasswordUpload">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" disabled={isCheckedUpload ? false : true} />
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      disabled={isCheckedUpload ? false : true}
+                    />
                   </Form.Group>
                 </Form.Group>
 
@@ -48,7 +120,7 @@ export default function Home() {
         </Card>
 
         <Card>
-        <Card.Body>
+          <Card.Body>
             <Form>
               <Form.Group>
                 <Form.Label>Download</Form.Label>
@@ -60,12 +132,23 @@ export default function Home() {
 
                 <Form.Group controleId="formPasswordDownload">
                   <Form.Group controlId="formPasswordSwitchDownload">
-                    <Form.Check type="switch" label={isCheckedDownload ? "Has password" : "No password" } checked={isCheckedDownload} onChange={(e)=>{setisCheckedDownload(e.target.checked)}} />
+                    <Form.Check
+                      type="switch"
+                      label={isCheckedDownload ? "Has password" : "No password"}
+                      checked={isCheckedDownload}
+                      onChange={(e) => {
+                        setisCheckedDownload(e.target.checked);
+                      }}
+                    />
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPasswordDownload">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" disabled={isCheckedDownload ? false : true} />
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      disabled={isCheckedDownload ? false : true}
+                    />
                   </Form.Group>
                 </Form.Group>
 
@@ -82,14 +165,13 @@ export default function Home() {
         </Card>
       </CardGroup>
 
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="danger" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
@@ -97,6 +179,98 @@ export default function Home() {
           </Button>
         </Modal.Footer>
       </Modal>
+      {loading ? (
+        <Spinner
+          animation="border"
+          variant="primary"
+          style={{ marginLeft: "50%", marginTop: "15%" }}
+        />
+      ) : (
+        <Container style={{ marginTop: "5%" }}>
+          <Row>
+            <h2>All files</h2>
+            <Table striped bordered hover responsive>
+              <thead>
+                <tr>
+                  <th>Uploader</th>
+                  <th>Filename</th>
+                  <th>Document ID</th>
+                  <th>Checksum</th>
+                </tr>
+              </thead>
+              <tbody>
+                {files.map((item, i) => (
+                  <UserRow
+                    key={i}
+                    item={item}
+                    onClick={handleUserClick.bind(this, item)}
+                  />
+                ))}
+              </tbody>
+            </Table>
+          </Row>
+          <Row>
+            <h2>Logs of selected file</h2>
+            <Table striped bordered hover responsive>
+              <thead>
+                <tr>
+                  <th>Uploader</th>
+                  <th>Document ID</th>
+                  <th>Log ID</th>
+                  <th>Log time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {logs.map((item, i) => (
+                  <tr key={i}>
+                    <td>{item.username}</td>
+                    <td>{item.DocID}</td>
+                    <td>{item.LogID}</td>
+                    <td>{item.LogTime}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
+      )}
     </div>
   );
 }
+
+/*  <Button variant="primary" onClick={handleShow}>
+                    Access logs
+                  </Button>
+
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Logs of file: {item.filename}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Table responsive>
+                        <thead>
+                          <tr>
+                            <th>Uploader</th>
+                            <th>Document ID</th>
+                            <th>Log ID</th>
+                            <th>Log time</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {logs.map((item, i) => (
+                            <tr key={i}>
+                              <td>{item.username}</td>
+                              <td>{item.DocID}</td>
+                              <td>{item.LogID}</td>
+                              <td>{item.LogTime}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </Table>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="danger" onClick={handleClose}>
+                        Close
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>*/
